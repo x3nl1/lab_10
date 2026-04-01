6de772419c69fd6ab689cf3d3864c9946cc08264
x3nl1 <stepanagapov2005@gmail.com>
2026-04-01 20:27:10 +0300

    feat(client): add jwt support

--- python_client/client.py
+++ python_client/client.py
@@ -4,3 +4,12 @@ BASE = "http://localhost:8080"
 
 def calculate(values):
     return requests.post(f"{BASE}/calculate", json={"values": values}).json()
+
+def get_token():
+    return requests.get(f"{BASE}/token").json()["token"]
+
+def protected(token):
+    return requests.get(
+        f"{BASE}/protected",
+        headers={"Authorization": token}
+    ).status_code
